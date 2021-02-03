1d81faeceed78346d1c2b35ef217f372bf59796a
lukasz biernacki <thelubb1@gmail.com>
2021-02-03 17:06:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+if($(window).width() > 576) {
+
+
+  $(function(){
+
+
+    let counter = 0;
+    let change;
+
+
+    if (counter == 0) {
+      $(".picture").on('click', (e)=> {
+      counter++;
+      for (var i = 1; i <= $(".picture").length; i++) {
+      if(e.target.id == "pic" + i) {
+        change = "#box" + i;
+        $(".text_box").css({"display": "none"})
+        $(change).css({'display':'flex' })
+
+        if (i<=5) {
+          change = "#box" + i;
+          $(change).css({'display':'flex' })
+          $(change).css({'grid-area':'' + 1 + ' / ' + i +' / '+ 1 + ' / ' + i })
+        }
+
+        if (i>=5) {
+          change = "#box" + i;
+          $(change).css({'display':'flex' })
+          $(change).css({'grid-area':'' + 2 + ' / ' + (i-5) +' / '+ 2 + ' / ' + (i-4) })
+        }
+
+      }
+    }})
+  }
+  $(document).on("click", (e)=> {
+    if(e.pageY < $(document).height() - $(".visual").height()) {
+        $(".text_box").css({'display':'none'})
+    }
+
+  })
+
+
+  $(".close").on("click", (e)=> {
+        $(".text_box").css({'display':'none'})
+  })
+
+  })
+
+//End of desktop script
+} else {
+
+
+
+
+//End of mobile script
+}
